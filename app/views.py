9cd0c4f48d98cbd8f4a6ca4da36e54241a38a2bf
Vanessa Lee <vklee@perx.com>
2015-09-08 23:16:13 -0500

    fix grammar

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from datetime import datetime
 YOUR_INFO = {
     'name' : 'Vanessa Lee',
     'bio' : 'I am a very, very lucky individual. I am a senior full-stack developer in a small tech department in Austin, TX. We code our data services in Erlang/Elixir/Phoenix and our front-end applications in Ember.js, Python, php, and now Django! In addition to that, I am a single-mother of three amazing boys and an agented fiction author writing books for children and young adults.' ,
-    'interests' : 'Currently I am digging deep into the best testing and documentation practices. My team has been lacking in these areas. My kids and I are addictvely listening to the audiobook of Ready Player One. I\'m reading The Boy in the Black Suit, and I\'ll be moderating some panels at the Texas Book Festival, so come and see me!', 
+    'interests' : 'Currently I am digging deep into the best testing and documentation practices. My team has been lacking in these areas. My kids and I are addictvely listening to the audiobook of Ready Player One. I\'m reading The Boy in the Black Suit, and I\'ll be moderating some panels at the Texas Book Festival. Come and see me!', 
     'email' : 'vanessaklee@gmail.com', # Leave blank if you'd prefer not to share your email with other conference attendees
     'twitter_username' : 'girlsleuthcom', # No @ symbol, just the handle.
     'github_username' : "vanessaklee", 
